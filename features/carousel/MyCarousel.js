6574c777e72861d228d2055487f7ee64d0cc8bc8
Frank <fang.oscar07@gmail.com>
2019-03-24 22:01:15 +0800

    feat(carousel): slider style carousel

--- features/carousel/MyCarousel.js
+++ features/carousel/MyCarousel.js
@@ -1,38 +1,59 @@
-import React from 'react';
-import Carousel from 'react-native-snap-carousel';
+import React from "react";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
+import Carousel from "react-native-snap-carousel";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 export default class MyCarousel extends React.Component {
+  static navigationOptions = {
+    title: "MyCarousel"
+  };
 
-    static navigationOptions = {
-        title: "MyCarousel",
+  constructor(props) {
+    super(props);
+    this.state = {
+      entries: [
+        { title: "Hello" },
+        { title: "Hello" },
+        { title: "Hello" },
+        { title: "World" },
+        { title: "World" },
+        { title: "World" }
+      ]
     };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            entries: 1
-        }
-    }
+  _renderItem({ item, index }) {
+    return (
+      <View style={styles.slider}>
+        <Text>{item.title}</Text>
+      </View>
+    );
+  }
 
-    _renderItem({ item, index }) {
-        return (
-            <View>
-                <Text>{item.title}</Text>
-            </View>
-        );
-    }
-
-    render() {
-        return (
-            <Carousel
-                ref={(c) => {
-                    this._carousel = c;
-                }}
-                data={this.state.entries}
-                renderItem={this._renderItem}
-                sliderWidth={120}
-                itemWidth={80}
-            />
-        );
-    }
+  render() {
+    return (
+      <Carousel
+        ref={c => {
+          this._carousel = c;
+        }}
+        data={this.state.entries}
+        renderItem={this._renderItem}
+        sliderWidth={SCREEN_WIDTH}
+        sliderHeight={200}
+        itemWidth={SCREEN_WIDTH / 2}
+        layout="tinder"
+      />
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  slider: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "tomato",
+    height: 200
+  }
+});
